8512b239e609aad35d49a4f4fae1eb831c70f7a7
daniil_stepanenko <daniil.stepanenko.96@gmail.com>
2021-09-20 16:02:22 +0300

    added benchmark

---
+++ app/app_bench_test.go
@@ -0,0 +1,30 @@
+package app_test
+
+import (
+	"testing"
+
+	"github.com/libmonsoon-dev/scanerr/app"
+	"github.com/libmonsoon-dev/scanerr/config"
+)
+
+func Benchmark(b *testing.B) {
+	conf := config.DefaultConfig()
+	conf.PackagesLoaderConf.Patterns = []string{"../testdata/file-not-found/cmd"}
+	s := app.NewScanerr(conf)
+
+	inputErr := "runtime error: open /not-exist: open /not-exist: file does not exist"
+	_, err := s.Scan(inputErr)
+	if err != nil {
+		b.Fatal(err)
+	}
+
+	b.ResetTimer()
+	b.ReportAllocs()
+
+	for i := 0; i < b.N; i++ {
+		_, err := s.Scan(inputErr)
+		if err != nil {
+			b.Fatal(err)
+		}
+	}
+}
